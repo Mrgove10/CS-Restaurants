e056b22edb06e3c2578ae63c9851f1c6219f4839
Adrien RICHARD <richardadrien0@gmail.com>
2019-11-28 10:19:02 +0100

    tests

--- App.Data.Test/JsonRestauTest.cs
+++ App.Data.Test/JsonRestauTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using App.Data.JsonRepository;
 using NUnit.Framework;
 
@@ -7,6 +8,8 @@ namespace App.Data.Test
     [TestFixture]
     public class JsonRestauTest
     {
+        JsonRestaurantRepository srv = new JsonRestaurantRepository();
+
         [SetUp]
         public void Setup()
         {
@@ -15,9 +18,38 @@ namespace App.Data.Test
         [Test]
         public void TestLoadData()
         {
-            var srv = new JsonRestaurantRepository();
-            var result = srv.LoadData("D:\\PERSO\\EPSI\\B3_(2019-2020)\\DotNet\\CS-Restaurants\\App.Data.Test\\Ressources\\restaurants.net.json");
+            var result = srv.LoadData(@".\Ressources\restaurants.net.json");
             Assert.IsTrue(result.Count == 25357);
+        }
+
+        [Test]
+        public void Test_Pizz()
+        {
+            var result = srv.LoadData(@".\Ressources\restaurants.net.json");
+            Assert.IsTrue(result.FindAll(x => 
+                              x.name.ToLower().Contains("pizz") && 
+                              x.borough.Equals("Manhattan")).Count == 437);
+        }
+
+        [Test]
+        public void Test_PizzGrade()
+        {
+            var result = srv.LoadData(@".\Ressources\restaurants.net.json");
+            Assert.IsTrue(result.FindAll(x => 
+                              x.name.ToLower().Contains("pizz") &&
+                              x.borough.Equals("Manhattan") && 
+                              x.grades.Any(g => g.grade == "A" && g.score <= 8)).Count == 274);
+        }
+        
+        
+        [Test]
+        public void Test_PizzGradeKitchen()
+        {
+            var result = srv.LoadData(@".\Ressources\restaurants.net.json");
+            Assert.IsTrue(result.FindAll(x => 
+                              x.name.ToLower().Contains("pizz") &&
+                              x.borough.Equals("Manhattan") && 
+                              x.grades.Any(g => g.grade == "A" && g.score <= 8)).Count == 274);
         }
     }
 }
